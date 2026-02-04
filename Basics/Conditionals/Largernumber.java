fc79cbeb9b5715a4a41a31c52d8e1e42d70300b4
subish-01 <subishsingh7@gmail.com>
2026-02-04 22:00:27 +0530

    Greater number check using conditional statements

---
+++ Basics/Conditionals/Largernumber.java
@@ -0,0 +1,21 @@
+package Conditionals;
+import java.util.Scanner;
+public class Largernumber 
+{
+    public static void main(String[] args) 
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num1 = scan.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = scan.nextInt();
+        if(num1 > num2)
+        {
+            System.out.println("The greatest number is "+num1);
+        }
+        else
+        {
+            System.out.println("The greatest number is "+num2);
+        }
+    }
+}
