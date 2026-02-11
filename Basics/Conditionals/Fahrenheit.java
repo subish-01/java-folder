8ce5de749787508d5c567055ae076847862c55b3
subish-01 <subishsingh7@gmail.com>
2026-02-11 22:15:29 +0530

    practiced fahrenheit to celcius problem

--- Basics/Conditionals/Fahrenheit.java
+++ Basics/Conditionals/Fahrenheit.java
@@ -1,9 +1,16 @@
 package Conditionals;
 
+import java.util.Scanner;
+
 public class Fahrenheit 
 {
     public static void main(String[] args) 
     {
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the fahrenheit in your area: ");
+        double fahrenheit = scan.nextDouble();
+        double celcius = (fahrenheit - 32)/1.8;
+        System.out.println("The celcius in your area is : "+celcius);
+        scan.close();
     }
 }
