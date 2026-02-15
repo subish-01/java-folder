15a4b089c8ba4bee83c67b3378f2433a3dd9b2b6
subish-01 <subishsingh7@gmail.com>
2026-02-15 19:44:15 +0530

    add LeftTriangle program

---
+++ Basics/LeftTriangle.java
@@ -0,0 +1,14 @@
+public class LeftTriangle 
+{
+    public static void main(String[] args)
+    {
+        for(int i = 1; i<=5; i++)
+        {
+            for(int j = 5-i+1;j>=1;j--)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
